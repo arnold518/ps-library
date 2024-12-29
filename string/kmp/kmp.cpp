10365222f5f03a7de33a22055621b9f6314216ea
arnold518 <arnoldpark03@gmail.com>
2024-12-29 23:10:00 +0900

    kmp

--- string/kmp/kmp.cpp
+++ string/kmp/kmp.cpp
@@ -1,13 +1,13 @@
 // kmp(S = "ababacaca") = [ -, 0, 1, 2, 3, 0, 1, 0, 1 ]
 vector<int> kmp(string S)
 {
-	int N=S.size();
-	vector<int> A(N);
-	for(int i=1; i<N; i++)
-	{
-		int j=A[i-1];
-		while(j>0 && S[j]!=S[i]) j=A[j-1];
-		if(S[j]==S[i]) A[i]=j+1;
-	}
-	return A;
+    int N=S.size();
+    vector<int> A(N);
+    for(int i=1; i<N; i++)
+    {
+        int j=A[i-1];
+        while(j>0 && S[j]!=S[i]) j=A[j-1];
+        if(S[j]==S[i]) A[i]=j+1;
+    }
+    return A;
 }
