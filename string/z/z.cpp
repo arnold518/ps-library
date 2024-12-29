da3e75d0893a097174ab55cc62d355c0bbc4d1fb
arnold518 <arnoldpark03@gmail.com>
2024-12-29 23:04:47 +0900

    z

--- string/z/z.cpp
+++ string/z/z.cpp
@@ -2,13 +2,13 @@
 // get_z(S = "ababacaca") = [ -, 0, 3, 0, 1, 0, 1, 0, 1 ]
 vector<int> get_z(string S)
 {
-    int N=S.size();
-    vector<int> A(N);
-    for(int i=1, j=0; i<N; i++)
-    {
-        if(j+A[j]>i) A[i]=min(j+A[j]-i, A[i-j]);
-        while(i+A[i]<N && S[A[i]]==S[i+A[i]]) A[i]++;
-        if(i+A[i]>j+A[j]) j=i;
-    }
-    return A;
+	int N=S.size();
+	vector<int> A(N);
+	for(int i=1, j=0; i<N; i++)
+	{
+		if(j+A[j]>i) A[i]=min(j+A[j]-i, A[i-j]);
+		while(i+A[i]<N && S[A[i]]==S[i+A[i]]) A[i]++;
+		if(i+A[i]>j+A[j]) j=i;
+	}
+	return A;
 }
