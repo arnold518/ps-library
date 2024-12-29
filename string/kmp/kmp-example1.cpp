3539c2ddb91377d52a085003a14f2c91e7459537
arnold518 <arnoldpark03@gmail.com>
2024-12-29 23:40:26 +0900

    kmp

--- string/kmp/kmp-example1.cpp
+++ string/kmp/kmp-example1.cpp
@@ -7,18 +7,21 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-// kmp(S = "ababacaca") = [ -, 0, 1, 2, 3, 0, 1, 0, 1 ]
-vector<int> kmp(string S)
+namespace KMP
 {
-    int N=S.size();
-    vector<int> A(N);
-    for(int i=1; i<N; i++)
+    // kmp(S = "ababacaca") = [ -, 0, 1, 2, 3, 0, 1, 0, 1 ]
+    vector<int> kmp(string S)
     {
-        int j=A[i-1];
-        while(j>0 && S[j]!=S[i]) j=A[j-1];
-        if(S[j]==S[i]) A[i]=j+1;
+        int N=S.size();
+        vector<int> A(N);
+        for(int i=1; i<N; i++)
+        {
+            int j=A[i-1];
+            while(j>0 && S[j]!=S[i]) j=A[j-1];
+            if(S[j]==S[i]) A[i]=j+1;
+        }
+        return A;
     }
-    return A;
 }
 
 int main()
@@ -30,7 +33,7 @@ int main()
     getline(cin, T);
 
     S=T+"?"+S;
-    auto V = kmp(S);
+    auto V = KMP::kmp(S);
     vector<int> ans;
     for(int i=0; i<V.size(); i++) if(V[i]==T.size()) ans.push_back(i-T.size()-T.size()+1);
 
