6084c35a6d9ae4e6f39abe6572950c9a0b4f6883
arnold518 <arnoldpark03@gmail.com>
2024-12-30 23:23:21 +0900

    dinic

--- optimization/dinic/dinic.cpp
+++ optimization/dinic/dinic.cpp
@@ -2,7 +2,7 @@ struct Dinic
 {
     // 0-based, directed graph, init : O(V), add_edge : O(1), flow : O(min(V^2 E, EF))
     // call init() first, add_edge(u, v, c), then flow(source, sink)
-    // adj = residual graph of current flow (v = node, c = capacity, r = index of reverse edge)
+    // adj = residual graph of current flow (v = node, c = remaining capacity, r = index of reverse edge)
     // to get actual flow, you must save original capcity of all edges
     typedef long long T;
     const static int MAXV = 1200;
