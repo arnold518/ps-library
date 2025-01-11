d51e7b9230c67f460b60afe7d3b3002a749b18d1
arnold518 <arnoldpark03@gmail.com>
2025-01-12 00:39:12 +0900

    ntt

--- math/ntt/ntt-example1.cpp
+++ math/ntt/ntt-example1.cpp
@@ -44,7 +44,8 @@ namespace NTT
 {
     const mint G = 3; // primitive root of MOD
 
-    // Get DFT (IDFT if inv is true) of polynomial F
+    // dft : O(N log N), 0-based
+    // get DFT (IDFT if inv is true) of polynomial F
     // length of A must be power of 2
     // dft([1, 2, 3, 4], false) = [10, 173167434, 998244351, 825076915]
     // dft([10, 173167434, 998244351, 825076915], true) = [1, 2, 3, 4]
@@ -83,7 +84,8 @@ namespace NTT
         }
     }
     
-    // Multiplies polynomial F, G
+    // multiply : O(N log N), 0-based
+    // multiplies polynomial F, G
     // multiply([1, 2, 3], [4, 5, 6, 7, 8]) = [4, 13, 28, 34, 40, 37, 24]
     vector<mint> multiply(vector<mint> F, vector<mint> G)
     {
